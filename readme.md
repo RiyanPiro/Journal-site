0969ec2b1ff42c63c4827777fa76d38241339be7
Riyan Dutta <90974133+RiyanPiro@users.noreply.github.com>
2024-03-08 13:18:20 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,24 @@
+# 📓 Journal Website
+
+## 🚀 Features
+- Saves content to the database in realtime as you type
+- Opens to the latest page
+- Left and Right buttons to navigate through pages
+- Delete button to delete the current page
+- Delete empty pages
+
+## ⚠️ Steps of Usage
+1. Install node.js
+2. Install MongoDB
+3. Add `C:\Program Files\MongoDB\Server\7.0\bin` to PATH
+4. Run `mongod` command
+5. Run `npm install mongodb` command in the project directory
+6. Run `npm install express` command in the project directory
+7. Run server.js
+8. Open http://localhost:3000/
+
+![Demo](demo.gif)
+
+## 💥 To be added
+- Find page by pageNumber/date/content
+- Animation for page flip
---
+++ server.js
@@ -0,0 +1,116 @@
+// Import libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+// Set up the server
+const url = 'mongodb://localhost:27017';
+const dbName = 'journal';
+const client = new MongoClient(url);
+
+// Connect the static files
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Get the latest page
+app.get('/latestPage', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    let latestPage = await db.collection('pages').find().sort({ pageNumber: -1 }).limit(1).toArray();
+    if (latestPage.length > 0) {
+      res.status(200).json(latestPage[0]);
+    } else {
+      res.status(404).send('No pages found');
+    }
+  }
+  catch (err) {
+    console.log(err.stack);
+    res.status(500).send('Error getting latest page');
+  }
+});
+
+// Display data in the page
+app.get('/getPage/:pageNumber', async (req, res) =>{
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    let page = await db.collection('pages').findOne({ pageNumber: parseInt(req.params.pageNumber) });
+    if (page) {
+      res.status(200).json(page);
+    } else {
+      res.status(404).send('Page not found');
+    }
+  }
+  catch (err) {
+    console.log(err.stack);
+    res.status(500).send('Error getting page data');
+  }
+});
+
+// Save changes made to the page
+app.post('/savePage', async (req, res) => {
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    let r = await db.collection('pages').findOneAndUpdate(
+      { pageNumber: req.body.pageNumber },
+      {
+        $setOnInsert: { date: req.body.date },
+        $set: { content: req.body.content }
+      },
+      { upsert: true }
+    );
+    res.status(200).send('Page data saved');
+  }
+  catch (err) {
+    console.log(err.stack);
+    res.status(500).send('Error saving page data');
+  }
+});
+
+// Delete the page by getting the page number
+app.post('/deletePage', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    let r = await db.collection('pages').findOneAndDelete(
+      { pageNumber: req.body.pageNumber }
+    );
+    res.status(200).send('Page deleted');
+  }
+  catch (err) {
+    console.log(err.stack);
+    res.status(500).send('Error deleting page');
+  }
+});
+
+// Delete documents with empty content
+app.post('/deleteEmpty', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    let r = await db.collection('pages').deleteMany(
+      { content: "" }
+    );
+    res.status(200).send('Empty pages deleted');
+  }
+  catch (err) {
+    console.log(err.stack);
+    res.status(500).send('Error deleting empty pages');
+  }
+});
+
+app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
